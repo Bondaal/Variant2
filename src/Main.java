a6f2d1bb0cc52108ed0d21779c808bcb7a98ad56
Bondaal <1bondareva.a@gmail.com>
2024-04-30 18:11:16 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+        public static void main(String[] args) {
+                int score = 100; // счет клиента
+                int deposit = 300; // пополнение
+
+                int bonus;
+
+                if (deposit > 1000) {
+                        bonus = deposit / 100;
+                } else {
+                        bonus = 0;
+                }
+                int balance = score + deposit +bonus;
+                System.out.println("Ваш счет:" + balance + "рублей");
+                System.out.println("Ваш бонус:" + bonus + "рублей");
+
 
         }
-    }
 }
